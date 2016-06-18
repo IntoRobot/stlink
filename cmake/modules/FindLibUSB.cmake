74845aceae6cbe354158031abca8395e8db92c06
Jerry Jacobs <jerry.jacobs@xor-gate.org>
2016-06-18 20:00:26 +0200

    Check MINGW and MSYS in FindLibUSB

--- cmake/modules/FindLibUSB.cmake
+++ cmake/modules/FindLibUSB.cmake
@@ -2,13 +2,13 @@
 # Once done this will define
 #
 #  LIBUSB_FOUND - System has libusb
-#  LIBUSB_INCLUDE_DIR - The libusb include directory
+#  LIBUSB_include_DIR - The libusb include directory
 #  LIBUSB_LIBRARY - The libraries needed to use libusb
 #  LIBUSB_DEFINITIONS - Compiler switches required for using libusb
 #
 #  Original from https://github.com/texane/stlink/blob/master/cmake/modules/FindLibUSB.cmake
 
-if(WIN32)
+if(WIN32 OR MINGW OR MSYS)
 	set(LIBUSB_WIN_VERSION "1.0.20")
 	set(LIBUSB_WIN_ARCHIVE libusb-${LIBUSB_WIN_VERSION}.7z)
 	set(LIBUSB_WIN_ARCHIVE_PATH ${CMAKE_BINARY_DIR}/${LIBUSB_WIN_ARCHIVE})
@@ -19,14 +19,15 @@ if(WIN32)
 	else ()
 		file(DOWNLOAD
 			https://sourceforge.net/projects/libusb/files/libusb-1.0/libusb-${LIBUSB_WIN_VERSION}/libusb-${LIBUSB_WIN_VERSION}.7z/download
-			${LIBUSB_WIN_ARCHIVE_PATH})
+			${LIBUSB_WIN_ARCHIVE_PATH}
+			SHOW_PROGRESS)
 	endif ()
 
 	execute_process(COMMAND ${ZIP_LOCATION} x -y ${LIBUSB_WIN_ARCHIVE_PATH} -o${LIBUSB_WIN_OUTPUT_FOLDER}
 	)
 endif()
 
-FIND_PATH(LIBUSB_INCLUDE_DIR NAMES libusb.h
+FIND_PATH(LIBUSB_include_DIR NAMES libusb.h
 	HINTS
 	/usr
 	/usr/local
@@ -52,7 +53,7 @@ FIND_LIBRARY(LIBUSB_LIBRARY NAMES ${LIBUSB_NAME}
 	${LIBUSB_WIN_OUTPUT_FOLDER}/MinGW64/static
 )
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Libusb DEFAULT_MSG LIBUSB_LIBRARY LIBUSB_INCLUDE_DIR)
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Libusb DEFAULT_MSG LIBUSB_LIBRARY LIBUSB_include_DIR)
 
-MARK_AS_ADVANCED(LIBUSB_INCLUDE_DIR LIBUSB_LIBRARY)
+MARK_AS_ADVANCED(LIBUSB_include_DIR LIBUSB_LIBRARY)
